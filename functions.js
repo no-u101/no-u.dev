3970c893de70f2508086af7aeefe301b244bc7b4
no-u <81192508+no-u101@users.noreply.github.com>
2022-06-05 21:26:18 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,16 @@
+function sendMessage(mess) {
+        webhook_url = "https://discord.com/api/webhooks/983222273081245726/YW9JOXOk42838M154hk9QIZgBA7n9p05-DqP0tBKaPnpc0PZ-Gtw7_-4FhxtsMghwb5L"
+        console.log(mess)
+        var request = new XMLHttpRequest();
+        request.open("POST", webhook_url);
 
+        request.setRequestHeader('Content-type', 'application/json');
+
+        var params = {
+                username: "no u",
+                avatar_url: "https://cdn.discordapp.com/avatars/687108508130213935/c58c1e5cb19c3f6a0a1d817d14713ab2.webp",
+                content: mess
+        }
+
+        request.send(JSON.stringify(params));
+}
